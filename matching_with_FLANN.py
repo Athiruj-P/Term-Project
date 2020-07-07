7db0bf4529b9baddc34588e5145d294da7bf13a4
Athiruj-CSI <athiruj@itpcl.co.th>
2020-07-07 15:32:38 +0700

    Add resize func and edit show image

--- matching_with_FLANN.py
+++ matching_with_FLANN.py
@@ -1,8 +1,32 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
-img1 = cv.imread('golden_grahams.png',cv.IMREAD_GRAYSCALE)          # queryImage
-img2 = cv.imread('products.png',cv.IMREAD_GRAYSCALE) # trainImage
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv.resize(image, dim, interpolation=inter)
+
+origin_image01 = cv.imread('Photo/cocoa_puffs_brownie_crunch.jpg')
+origin_image02 = cv.imread('Photo/products.png')
+
+if origin_image01.shape[1] > 400:
+    origin_image01 = ResizeWithAspectRatio(origin_image01,width = 400)
+
+img1 = cv.cvtColor(origin_image01, cv.COLOR_BGR2GRAY)
+img2 = cv.cvtColor(origin_image02, cv.COLOR_BGR2GRAY)
+
+
 
 # Initiate SIFT detector
 sift = cv.xfeatures2d.SIFT_create()
@@ -13,7 +37,7 @@ kp2, des2 = sift.detectAndCompute(img2,None)
 # เลือก Algorithm เป็น KDTREE 
 FLANN_INDEX_KDTREE = 1
 # trees คือจำนวนการทำนายแบบขนานอยู่ระหว่าง ซึ่งมีค่าแนะนำที่ 1 - 16
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 10)
 # checks คือจำนวนครั้งการท่อง (Traversals) ในแต่ละ tree 
 # ยิ่งมีค่ามากจะมีความแม่นยำสูง แต่ใช้ทรัพยากรเครืองมากเช่นกัน
 search_params = dict(checks=50)   # or pass empty dictionary
@@ -28,8 +52,10 @@ for i,(m,n) in enumerate(matches):
     if m.distance < 0.7*n.distance:
         matchesMask[i]=[1,0]
 draw_params = dict(matchColor = (0,255,0),
-                   singlePointColor = (255,0,0),
+                   singlePointColor = (0,0,255),
                    matchesMask = matchesMask,
                    flags = cv.DrawMatchesFlags_DEFAULT)
-img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-plt.imshow(img3,),plt.show()
+img3 = cv.drawMatchesKnn(origin_image01,kp1,origin_image02,kp2,matches,None,**draw_params)
+cv.imshow("Image", img3)
+cv.waitKey(0)    
+# plt.imshow(img3,),plt.show()
