d1a5799e2d3266c77b339806e1727c2cce356d5d
Athiruj-CSI <athiruj@itpcl.co.th>
2020-07-10 09:47:43 +0700

    Comment all func

--- image_remove_bg_and_size_measurement.py
+++ image_remove_bg_and_size_measurement.py
@@ -24,13 +24,16 @@ import cv2
 def midpoint(ptA, ptB):
     return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-# ฟังก์ชันปรับขนาดรูปภาพตามพารามิเตอร์ที่ส่งค่ามาเป็น Pixel
 
-
+# ResizeWithAspectRatio
+# ฟังก์ชันปรับขนาดของภาพตามที่กำหนด (Pixel) ตามอัตราส่วนของภาพ
+# Input : Image , Width and/or hight in pixel
+# Output : Resized image
+# Create date : 07-July-2020
+# Auther : Athiruj Poositaporn
 def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
-
     if width is None and height is None:
         return image
     if width is None:
@@ -38,23 +41,25 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
         dim = (int(w * r), height)
     else:
         r = width / float(w)
-        dim = (width, int(h * r))
-
+        dim = (width, int(h * r)
     return cv2.resize(image, dim, interpolation=inter)
 
-
+# get_background_mask
+# ฟังก์ชัน mask สีพื้นหลังตามภาพและสีที่กำหนด
+# Input : Image , background color
+# Output : Mask
+# Create date : 07-July-2020
+# Auther : Athiruj Poositaporn
 def get_background_mask(image, color):
     if color == "red":
         # คาบของสีแดงมี 2 ช่วง จึงจะครอบคลุม
-        mask01 = cv2.inRange(image, np.array(
-            [0, 49, 19]), np.array([5, 255, 255]))
-        mask02 = cv2.inRange(image, np.array(
-            [175, 50, 20]), np.array([180, 255, 255]))
+        mask01 = cv2.inRange( image, np.array([0, 49, 19]), np.array([5, 255, 255]) )
+        mask02 = cv2.inRange( image, np.array([175, 50, 20]), np.array([180, 255, 255]) )
         return cv2.bitwise_or(mask01, mask02)
     elif color == "green":
-        return cv2.inRange(image, np.array([39, 23, 111]), np.array([102, 255, 255]))
+        return cv2.inRange( image, np.array([39, 23, 111]), np.array([102, 255, 255]) )
     elif color == "blue":
-        return cv2.inRange(image, np.array([94, 80, 2]), np.array([126, 255, 255]))
+        return cv2.inRange( image, np.array([94, 80, 2]), np.array([126, 255, 255]) )
     else:
         return False
 
@@ -62,28 +67,28 @@ def get_background_mask(image, color):
 # LOC : 24 - 29  =>รับพารามิเตอร์ 2 ค่าคือ
 # 1. Path ไปของไฟล์รูป
 # 2. ขนาดของวัถุที่ใช้อ้างอิง
-ap = argparse.ArgumentParser()
+ap=argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
                 help="path to the input image")
 ap.add_argument("-c", "--color", required=True,
                 help="color of background (red green or blue)")
 ap.add_argument("-w", "--width", type=float, required=True,
                 help="width of the left-most object in the image (in millimeter)")
-args = vars(ap.parse_args())
+args=vars(ap.parse_args())
 
 # นำเข้ารูปภาพจาก Path ที่ใส่มา
-image = cv2.imread(args["image"])
-image = ResizeWithAspectRatio(image, width=1080)
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+image=cv2.imread(args["image"])
+image=ResizeWithAspectRatio(image, width=1080)
+hsv=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-mask = get_background_mask(hsv, args["color"])
+mask=get_background_mask(hsv, args["color"])
 
 # cv2.Canny => การตีกรอบให้กับภาพ
-edged = cv2.Canny(mask, 50, 100)
+edged=cv2.Canny(mask, 50, 100)
 # dilate => ขยายเส้นขอบให้ใหญ่ขึ้น
-edged = cv2.dilate(edged, None, iterations=1)
+edged=cv2.dilate(edged, None, iterations=1)
 # dilate => ลบ Noise สีขาวออกจากรูปภาพ
-edged = cv2.erode(edged, None, iterations=1)
+edged=cv2.erode(edged, None, iterations=1)
 
 cv2.imshow("edged", edged)
 # writeImage(edged,"bg_green","eraser")
@@ -91,39 +96,36 @@ cv2.imshow("edged", edged)
 
 # ค้นหารูปร่างของวัตถุ
 # cv2.findContours(รุูปภาพ,ดึงเส้นขอบของวัตถุ,การประมาณการรูปร่างของวัตถุ)
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+cnts=cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
                         cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
+cnts=imutils.grab_contours(cnts)
 
 # เรียงลำดับเส้นขอบของวัตถุ เพื่อสามารถเข้าถึงวัตถุอ้างอิงได้ และสามารถ
 # หาค่าของ pixelsPerMetric จากวัตถุอ้างอิงดังกล่าว
-(cnts, _) = contours.sort_contours(cnts)
+(cnts, _)=contours.sort_contours(cnts)
 
 # กำหนดให้ pixelsPerMetric เป็น None ซึ่งใช้ในการคำนวณอัตราส่วนของรูปภาพอ้างอิง
 # เพื่อใช้ค้นหาความยาวด้านของวัตถุเป้าหมาย
-pixelsPerMetric = None
+pixelsPerMetric=None
 
-origin = image.copy()  # ลบลบบรรทัดนี้ออกถ้าต้องการให้แสดงผลลัพธ์ต่อ 1 obj
+origin=image.copy()  # ลบลบบรรทัดนี้ออกถ้าต้องการให้แสดงผลลัพธ์ต่อ 1 obj
 
-# loop เพื่อคำนวณความยาวด้านของแต่ละรูปทรงที่ค้นหาได้จากรูปภาพ
+# loop เพื่อคำนวณความยาวด้านของแต่ละรูปร่างที่ค้นหาได้จากรูปภาพ
 for c in cnts:
-    # cv2.contourArea => คืนค่าพื้นที่ของรูปทรงที่ c มีหน่วยคือ pixel^2
-    # ถ้าพื้นที่มีขนาดที่เล็กเกินไป จะข้ามไปยังรูปทรงถัดไป
+    # cv2.contourArea => คืนค่าพื้นที่ของรูปร่างที่ c มีหน่วยคือ pixel^2
+    # ถ้าพื้นที่มีขนาดที่เล็กเกินไป จะข้ามไปยังรูปร่างถัดไป
     # (cv2.contourArea(c)/args["width"]*args["width"]) คือการแปลงหน่วยจาก pixel^2 เป็น MM^2
     if (cv2.contourArea(c)/args["width"]*args["width"]) < 300:
-        # print("cv2.contourArea(c) => " + str(cv2.contourArea(c)))
-        # print("cv2.contourArea(c)/args['width']*args['width'] => " + str(cv2.contourArea(c)/args["width"]*args["width"]))
-        # print("###################################")
         continue
 
     # คำนวณหาเส้นรอบรูปรางของวัตถุที่มีความเอียง
     # origin = image.copy() # นำ comment ออกถ้าต้องการให้แสดงผลลัพธ์ต่อ 1 obj
-    box = cv2.minAreaRect(c)
-    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-    box = np.array(box, dtype="int")
+    box=cv2.minAreaRect(c)
+    box=cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    box=np.array(box, dtype="int")
 
     # วางเส้นรอบรูปของวัตถุโดยมีลำดับคือ บนซ้าย บนขวา ล่างซ้าย ล่างขวา
-    box = perspective.order_points(box)
+    box=perspective.order_points(box)
     cv2.drawContours(origin, [box.astype("int")], -1, (0, 255, 0), 2)
 
     # วาดรูปจุด (point) ในแต่ละมุมของเส้นรอบรูป
@@ -132,14 +134,14 @@ for c in cnts:
 
     # นำจุดทั้ง 4 จุด เก็บไว้ใน (tl, tr, br, bl) เพื่อคำนวณหาจุดกึ่งกลาง
     # ระหว่างจุดด้านบน (บนซ้าย บนขวา) และระหว่างจุดด้านล่าง (่ล่างซ้าน ล่างขวา)
-    (tl, tr, br, bl) = box
-    (tltrX, tltrY) = midpoint(tl, tr)
-    (blbrX, blbrY) = midpoint(bl, br)
+    (tl, tr, br, bl)=box
+    (tltrX, tltrY)=midpoint(tl, tr)
+    (blbrX, blbrY)=midpoint(bl, br)
 
     # คำนวณจุดกึ่งกลางระหว่าง บนซ้าย ล่างซ้าย
     # และ บนขวา ล่างขวา
-    (tlblX, tlblY) = midpoint(tl, bl)
-    (trbrX, trbrY) = midpoint(tr, br)
+    (tlblX, tlblY)=midpoint(tl, bl)
+    (trbrX, trbrY)=midpoint(tr, br)
 
     # วาดรูปจุดกึ่งกลาง (middle point) จากที่คำนวณข้างต้น
     cv2.circle(origin, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
@@ -154,16 +156,16 @@ for c in cnts:
              (255, 0, 255), 2)
 
     # คำนวณหาระยะหว่างระหว่างจุด (Euclidean distance)
-    dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-    dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+    dA=dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    dB=dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
     # กรณีที่ไม่มีการกำหนดค่า pixelsPerMetric จะนำค่าที่ได้จากพารามิเตอร์มาคำนวณหาอัตราส่วน
     if pixelsPerMetric is None:
-        pixelsPerMetric = dB / args["width"]
+        pixelsPerMetric=dB / args["width"]
 
     # คำนวณหาความยาวด้านของวัตถุ
-    dimA = dA / pixelsPerMetric
-    dimB = dB / pixelsPerMetric
+    dimA=dA / pixelsPerMetric
+    dimB=dB / pixelsPerMetric
 
     # แสดงตัวเลขจากการคำนวณ
     cv2.putText(origin, "{:.4f}mm".format(dimA),
