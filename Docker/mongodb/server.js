93475ca28e0480cb79d0bb31749ed4697673150d
Athiruj Poositaporn <athiruj@itpcl.co.th>
2020-08-03 14:37:36 +0700

    Test docker

---
+++ Docker/mongodb/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const connectDb = require("./src/connection");
+
+const PORT = 8080;
+
+app.get("/users", (req, res) => {
+  res.send("Get users \n");
+});
+
+app.get("/user-create", (req, res) => {
+  res.send("User created \n");
+});
+
+app.listen(PORT, function() {
+  console.log(`Listening on ${PORT}`);
+
+  connectDb().then(() => {
+    console.log("MongoDb connected");
+  });
+})
