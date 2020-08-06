53e96024cc6fd2716ece09d4e6f8ae18116fbf14
Athiruj Poositaporn <athiruj@itpcl.co.th>
2020-08-06 12:30:42 +0700

    Edit host ip

--- Docker/Flask/app/db_config.py
+++ Docker/Flask/app/db_config.py
@@ -1,7 +1,7 @@
 item = {
     "db_username" : "admin_mongo",
     "db_password" : "admin_mongo",
-    "db_host" : "localhost:27017",
+    "db_host" : "172.27.0.2",
     "db_name": "DPML_db",
     "db_col_user" : "dpml_user"
 }
