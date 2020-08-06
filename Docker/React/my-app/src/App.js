f8839cd42397b4cf43eaa58536bcfe27b38f839e
Athiruj Poositaporn <athiruj@itpcl.co.th>
2020-08-06 17:55:45 +0700

    Test react

--- Docker/React/my-app/src/App.js
+++ Docker/React/my-app/src/App.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'
+// import './App.css';
+class App extends Component {
+  getData(){
+    axios.get('http://localhost:5001/get_all_user')
+      .then(res=>{
+        console.log(res.data)
+      })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello</p>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <button onClick={()=>this.getData()}>Get all user</button>
+        <br />
+        <br />
+        <form>
+          <label>Username : </label>
+          <input type="text"></input>
+          <br />
+          <label>password : </label>
+          <input type="password"></input>
+          <br />
+          <label>role : </label>
+          <input type="number"></input>
+          <br />
+          <label>status : </label>
+          <input type="number"></input>
+          <br /><br />
+          <button type="submit">add new user</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
