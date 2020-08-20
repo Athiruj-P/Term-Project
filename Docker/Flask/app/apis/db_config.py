9a836342d968e92484a36a11f1d860162f96359d
Athiruj Poositaporn <athiruj@itpcl.co.th>
2020-08-20 17:50:51 +0700

    Add config items

--- Docker/Flask/app/apis/db_config.py
+++ Docker/Flask/app/apis/db_config.py
@@ -1,11 +1,31 @@
 item = {
     "db_username" : "admin_mongo",
     "db_password" : "admin_mongo",
-    "db_host" : "172.27.0.2",
+    "db_host" : "localhost:27017",
     "db_name": "DPML_db",
     "db_col_user" : "dpml_user",
     "db_col_role" : "dpml_role",
     "db_col_mlmo" : "dpml_ml_model",
     "db_col_remo" : "dpml_ref_model",
     "db_col_unit" : "dpml_unit",
+    
+    # dpml_ml_model fields
+    "fld_mlmo_id" : "mlmo_id",
+    "fld_mlmo_name" : "mlmo_name",
+    "fld_mlmo_path" : "mlmo_path",
+    "fld_mlmo_status" : "mlmo_status",
+    "fld_mlmo_status_delete" : 0,
+    "fld_mlmo_status_active" : 1,
+    "fld_mlmo_status_disable" : 2,
+    
+    # dpml_ref_model fields
+    # "fld_remo_id" : "remo_id",
+    # "fld_remo_name" : "remo_name",
+    # "fld_remo_path" : "remo_path",
+    # "fld_remo_status" : "remo_status",
+    # "fld_remo_status_default" : 0,
+
+    "db_file_path" : "db_file",
+    "ml_path" : "ml_model",
+    "ref_path" : "ref_model",
 }
