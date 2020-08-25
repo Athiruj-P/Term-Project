5558f08968908b84929bb0837e6dac265f406b4a
Athiruj Poositaporn <athiruj@itpcl.co.th>
2020-08-25 17:41:41 +0700

    Add log config file

---
+++ Docker/Flask/app/logging_config.py
@@ -0,0 +1,68 @@
+from datetime import date , datetime
+today = date.today()
+date_folder = today.strftime("%Y-%m-%d")
+
+dict_config = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s [%(levelname)s:%(name)s] {%(funcName)s:%(lineno)d} [SYSTEM]:%(message)s',
+        },
+        'user': {
+            'format': '%(asctime)s [%(levelname)s:%(name)s] {%(funcName)s:%(lineno)d} [USER]:%(message)s',
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': "log/{}.log".format(date_folder),
+            'maxBytes': 5000000,
+            'backupCount': 10
+        },
+        'user': {
+            'level': 'DEBUG',
+            'formatter': 'user',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': "log/{}.log".format(date_folder),
+            'maxBytes': 5000000,
+            'backupCount': 10
+        },
+        'console': {
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'main': {
+            'handlers': ["default"],
+            'level': 'DEBUG',
+        },
+        'model_management_api': {
+            'handlers': ["default"],
+            'level': 'DEBUG',
+        },
+        'MLManager_obj': {
+            'handlers': ["default"],
+            'level': 'DEBUG',
+        },
+        'log_management_api': {
+            'handlers': ["default"],
+            'level': 'DEBUG',
+        },
+        'LogManager': {
+            'handlers': ["default"],
+            'level': 'DEBUG',
+        },
+        'user_management' : {
+            'handlers': ["user"],
+            'level': 'DEBUG',
+        }
+    },
+    'root': {
+        'handlers': ["console"],
+        'level': 'DEBUG',
+    },
+}
