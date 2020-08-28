e22a1f8ec92a60993a360b0eb4b6f966a8096429
Athiruj Poositaporn <athiruj@itpc.co.th>
2020-08-29 01:51:18 +0700

    Edit

--- Docker/Flask/app/apis/image_processing/image.py
+++ Docker/Flask/app/apis/image_processing/image.py
@@ -1,7 +1,5 @@
 import cv2
 class Image:
-    def __init__(self, width=0 , height=0 ,image=cv2,measurement_unit=0):
-        self.width = width
-        self.height = height
+    def __init__(self ,image=cv2,measurement_unit=0):
         self.image = image
         self.measurement_unit = measurement_unit
